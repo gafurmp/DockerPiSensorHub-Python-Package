36b9678cb46a71d6b8144b924936bd61a5091c5d
gafurmp <gafurmp@gmail.com>
2020-06-09 15:13:10 +0200

    updated init

--- dockerpi/__init__.py
+++ dockerpi/__init__.py
@@ -9,7 +9,7 @@ Questions, comments? gafurmp@gmail.com
 """
 
 __author__ = 'Abdul Gafoor Mannoo PArambil <gafurmp@gmail.com>'
-__version__ = '0.1'
+__version__ = '0.2'
 
-from .dockerpi import *
+from .dockerpi import DockerPi
 from .consts import *
